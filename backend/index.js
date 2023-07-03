39b3745b0a499de68c93323d1eb2c2623cd52d6e
Nikhil Gupta <Gup7nik@gmail.com>
2023-07-03 17:53:01 +0530

    done5

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-dusky.vercel.app/",
 		credentials:true,
 		optionSuccessStatus:200,
 	})
